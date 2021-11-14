5acf1360f6483ecbe4f05edb098a076a3e870a72
S3Prototype <liuqahs15@gmail.com>
2021-11-14 11:09:29 -0600

    Integrate livepeer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
