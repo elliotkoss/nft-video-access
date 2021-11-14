15c4614a68b49d0430290d829344edef5b3220b8
jordang7 <77jgreenberg77@gmail.com>
2021-11-14 15:49:50 -0500

    remove unnecessary condition in script

--- public/js/unlock.js
+++ public/js/unlock.js
@@ -5,18 +5,16 @@ function unlockSetup() {
     js.src = "https://paywall.unlock-protocol.com/static/unlock.latest.min.js";
     sc.parentNode.insertBefore(js, sc);
   })(document, "script");
-  if (window.unlockProtocolConfig === undefined) {
-    window.unlockProtocolConfig = {
-      network: 4,
-      locks: {
-        "0xE27944a3613225FAfdE0a5229E680aEc0E3aA5aD": {
-          name: "Test Livestream",
-        },
+  window.unlockProtocolConfig = {
+    network: 4,
+    locks: {
+      "0x40eC871df1898C308AE8e965031eAE26d5a7Ca45": {
+        name: "Test Livestream",
       },
-      icon: "https://unlock-protocol.com/static/images/svg/unlock-word-mark.svg",
-      callToAction: {
-        default: "Unlock the Livestream here!",
-      },
-    };
-  }
+    },
+    icon: "https://unlock-protocol.com/static/images/svg/unlock-word-mark.svg",
+    callToAction: {
+      default: "Unlock the Livestream here!",
+    },
+  };
 }
