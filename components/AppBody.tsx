2f13d2907e25559277a4939d743a4e04faa35fda
S3Prototype <liuqahs15@gmail.com>
2021-11-15 08:07:54 -0600

    Fixed a small type error

--- components/AppBody.tsx
+++ components/AppBody.tsx
@@ -16,11 +16,13 @@ interface Props {
 }
 
 const copyTextToClipboard = (text: string) => {
-  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-    if (result.state == "granted" || result.state == "prompt") {
-      navigator.clipboard.writeText(text);
-    }
-  });
+  navigator.permissions
+    .query({ name: "clipboard-write" as PermissionName })
+    .then((result) => {
+      if (result.state == "granted" || result.state == "prompt") {
+        navigator.clipboard.writeText(text);
+      }
+    });
 };
 
 const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
