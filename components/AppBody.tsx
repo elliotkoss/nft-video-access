b81095082b8ad5b6c386bc5debe8759f3b5996a7
S3Prototype <liuqahs15@gmail.com>
2021-11-15 13:20:37 -0600

    Final fix for typescript clipboard issue

--- components/AppBody.tsx
+++ components/AppBody.tsx
@@ -16,13 +16,7 @@ interface Props {
 }
 
 const copyTextToClipboard = (text: string) => {
-  navigator.permissions
-    .query({ name: "clipboard-write" as PermissionName })
-    .then((result) => {
-      if (result.state == "granted" || result.state == "prompt") {
-        navigator.clipboard.writeText(text);
-      }
-    });
+  navigator.clipboard.writeText(text);
 };
 
 const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
