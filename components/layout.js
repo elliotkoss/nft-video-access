48dcbc1505c7cf5a537469746ea951eda5438cac
jordang7 <77jgreenberg77@gmail.com>
2021-11-18 21:10:32 -0500

    correct name spelling

--- components/layout.js
+++ components/layout.js
@@ -14,15 +14,24 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-      </header>
+      <header className={styles.header}></header>
       <main>{children}</main>
       <footer className={styles.footer}>
-        <div><a href="https://github.com/elliotkoss/nft-video-access" target="_blank" ><img src="/images/github-button.svg" width="91" height="51" /></a></div>
+        <div>
+          <a
+            href="https://github.com/elliotkoss/nft-video-access"
+            target="_blank"
+          >
+            <img src="/images/github-button.svg" width="91" height="51" />
+          </a>
+        </div>
         <div>Built by</div>
-        <div>Elliot Koss, Shaquil Hansford, Jordan Green,<br /> Aaron Surty, and Marcus Oshodi</div>
+        <div>
+          Elliot Koss, Shaquil Hansford, Jordan Greenberg,
+          <br /> Aaron Surty, and Marcus Oshodi
+        </div>
         <div className={styles.nftAgreement}>Our NFT Agreement</div>
       </footer>
     </div>
-  )
+  );
 }
